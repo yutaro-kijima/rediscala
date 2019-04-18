28a237c5ebd0c7e5510fbe6272c7cdfc0ea13060
yutaro-kijima <yutaro.kijima@geniee.co.jp>
2019-04-18 18:46:01 +0900

    プロトコルからコマンドを読み取るところまで完成

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,41 +1,61 @@
 import java.net._
+
 import scala.io._
 import java.io._
-val CRLF = "\r\n"
+import java.util.Scanner
+
 object Server {
+  val CRLF = "\r\n"
 
   def main(args: Array[String]): Unit = {
     //サーバーソケットを生成しポート番号をバインドする
     val serverSocket = new ServerSocket(4545)
-    println("Serve on host:localhost port:4545 ...")
     val socket: Socket = serverSocket.accept()
+    println("Serve on host:localhost port:4545 ...")
     while (true) {
-      //val text = lines.mkString("\n")
-      //val stream: java.io.InputStream = new java.io.ByteArrayInputStream(text.getBytes(java.nio.charset.StandardCharsets.UTF_8.name))
-      val input = new  BufferedSource(socket.getInputStream).getLines().mkString("\n")
+      val input = new BufferedSource(socket.getInputStream()).getLines()
       val output = new PrintStream(socket.getOutputStream)
-      println(input)
-      output.print("+PONG"+CRLF)
+      println(getInput(input, Nil))
+      output.print("+PONG" + CRLF)
       output.flush()
+    }
+    socket.close()
+  }
+
+  /*
+  def exec(str:String): String = {
+  }
+  */
+
+  //CRLFでsplitされたPESP配列からコマンドだけの配列を返す [*3,$5,index,$4,desc,$2,ss]
+  def getInput(input: Iterator[String], commands: List[Any]): List[Any] = {
+    val head = input.next()
+    head.toCharArray.toList match {
+      case List('+', _*) => List(head)
+      case Nil => commands
+      case List('*', i, _*) => getInputWithLength(input, Nil, i - '0')
+      case List('$', '-', '1', _*) => getInput(input, commands :+ null)
+      case List('$', _*) => getInput(input, commands :+ input.next())
+      case _ => getInput(input, commands :+ head.toInt)
     }
   }
 
-/*
-  def exec(str:String): String = {
+  def getInputWithLength(input: Iterator[String], commands: List[Any], length: Int): List[Any] = {
+    if (commands.length == length) {
+      return commands
+    } else {
+      val head = input.next()
+      head.toCharArray.toList match {
+        case Nil => commands
+        case List('$', '-', '1', _*) => getInputWithLength(input, commands :+ null, length)
+        case List('$', _*) => getInputWithLength(input, commands :+ input.next(), length)
+        case _ => getInputWithLength(input, commands :+ head.toInt, length)
+      }
+    }
+
   }
 
-  //CRLFでsplitされたPESP配列からコマンドだけの配列を返す [*3,$5,index,$4,desc,$2,ss]
-  def loop(lst: List[String], commands: List[Any], index: Int): List[Any] = lst(index).asInstanceOf[List[Char]] match {
-    case List('+', _*)            => List(lst(0))
-    case Nil                      => commands
-    case List('*', _*)            => loop(lst, commands, index + 1)
-    case List('$', '-', '1', _*)  => loop(lst, commands :+ null, index + 1)
-    case List('$', _*)            => loop(lst, commands :+ lst(index + 1), index + 2)
-    case _                        => loop(lst, commands :+ lst(index).toInt, index + 1)
-  }
+  def exec(commandsList: List[String]): String = {
 
-//  def exec(commandsList: List[String]): String = {
-//
-/ }
-*/
+  }
 }
