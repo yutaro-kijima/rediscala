1937e2fb448d48868c076ca210aa50ef47ba292e
yutaro-kijima <yutaro.kijima@geniee.co.jp>
2019-04-22 19:02:52 +0900

    exceed

--- src/main/scala/Process.scala
+++ src/main/scala/Process.scala
@@ -5,10 +5,29 @@ import Store
 
 class Process(socket:Socket) extends Thread {
 
+  val CRLF = "\r\n"
   override def run(): Unit = {
 
     val input: Iterator[String] = 
-      new BufferedSource 
+      new BufferedSource(socket.getInputStream()).getLines()
+    val output = new PrintStream(socket.getOutputStream)
+    while (true) {
+      if(input.hasNext) {
+        val commands = getInput(input, Nil)
+        if(commands != Nil){
+          val res = encode(exec(commands))
+          output.print(res + CRLF)
+          output.flush()
+        }
+      }
+    }
   }
+
+  def exec(commands: List[Any]): Any = 
+    commands.head.asInstanceOf[String].toUpperCase match {
+      case "PING" => if (commands.length == !) "PONG" else commands(1)
+      case "SET"  => onSet(commands.drop(1))
+
+    }
 }
 
